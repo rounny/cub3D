789764ec25e6bdead86ecfaea51a8f46d4a231d3
Lakiesha Emmon <crazyrounny@gmail.com>
2022-06-07 19:07:15 +0300

    add ft_calloc_int

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lemmon <lemmon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 10:52:48 by lemmon            #+#    #+#             */
-/*   Updated: 2022/03/19 10:52:48 by lemmon           ###   ########.fr       */
+/*   Updated: 2022/06/06 14:15:21 by lemmon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,24 @@ void	*ft_calloc(size_t count, size_t size)
 	while (i < count * size)
 	{
 		((unsigned char *) mem)[i] = '\0';
-		i++;
+		++i;
+	}
+	return (mem);
+}
+
+void	*ft_calloc_int(size_t count, int sym)
+{
+	size_t	i;
+	int		*mem;
+
+	i = 0;
+	mem = malloc(count * sizeof(int));
+	if (!mem)
+		return (NULL);
+	while (i < count)
+	{
+		mem[i] = sym;
+		++i;
 	}
 	return (mem);
 }
